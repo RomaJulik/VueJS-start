ddf481ba7f2cbc57a009ea893e43544e3cc22cf8
RomaJulik <roelkov@gmail.com>
2021-04-04 17:06:58 +0200

    fix checkbox

--- Vue_1/js/app.js
+++ Vue_1/js/app.js
@@ -1,26 +1,47 @@
 const App = {
     data() {
         return {
-            todoList: ['One', 'Two', 'Three'],
+            needDoList: [],
+            completeList: [],
+            inputValue: '',
             counter: 0,
             appTitle: 'TooToDo',
             placeholderString: "Введите дело которое нужно сделать...",
-            inputValue: '',
-            noTodo: 'Место для ваших задач'
+            noTodo: 'Место для ваших задач',
+            id: 0,
         }
     },
     methods: {
-        
+
         addNewTodo() {
+            this.id = ++this.id
             if (this.inputValue !== '') {
-                this.todoList.push(this.inputValue)
+                this.needDoList.push({
+                    title: this.inputValue,
+                    checked: false,
+                    id: this.id
+                });
                 this.inputValue = ''
+                console.log(this.needDoList);
+                console.log(this.completeList);
             }
 
         },
+
         removeTodo(index) {
-            this.todoList.splice(index, 1)
-            console.log('removeTod', this.todoList);
+            this.needDoList.splice(index, 1)
+            this.completeList.splice(index, 1)
+        },
+
+        doCheck(type, index) {
+            console.log(type, index);
+            if (type === true) {
+                const comleteTodo = this.needDoList.splice(index, 1)
+                this.completeList.push(...comleteTodo)
+            } else {
+                const noComleteTodo = this.completeList.splice(index, 1)
+                this.needDoList.push(...noComleteTodo)
+            }
         }
     }
 }
