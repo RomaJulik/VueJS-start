08518b079ef5a51fd494f851c2975e246460f910
RomaJulik <roelkov@gmail.com>
2021-04-06 00:45:35 +0200

    fix bug

--- Vue_1/js/app.js
+++ Vue_1/js/app.js
@@ -10,21 +10,19 @@ const App = {
             noTodo: 'Место для ваших задач',
             id: 0,
         }
-    
+
 
     },
 
-    async mounted () {
+    async mounted() {
         const needData = await localStorage.getItem('needDoList')
         const competeData = await localStorage.getItem('completeList')
-        
+
         if (needData || competeData) {
             this.needDoList = JSON.parse(needData)
             this.completeList = JSON.parse(competeData)
         }
-        console.log(needData)
-        console.log(competeData)
-    
+
     },
 
     methods: {
@@ -40,31 +38,27 @@ const App = {
                 localStorage.setItem('needDoList', JSON.stringify(this.needDoList))
                 this.inputValue = ''
             }
-            
+
         },
-        
-        removeNeedTodo(index) {
-            this.needDoList.splice(index, 1)
-            localStorage.setItem('needDoList', JSON.stringify(this.needDoList))
+
+        remove(index, store) {
+            this[store].splice(index, 1)
+            localStorage.setItem([store], JSON.stringify(this[store]))
         },
-        removeDoneTodo(index) {
-            this.completeList.splice(index, 1)
-            localStorage.setItem('completeList', JSON.stringify(this.completeList))
-        },
-        
-        clearAllTodo(){
+
+        clearAllTodo() {
             this.needDoList = []
             this.completeList = []
             localStorage.setItem('needDoList', JSON.stringify(this.needDoList))
             localStorage.setItem('completeList', JSON.stringify(this.completeList))
         },
 
-        
+
         doCheck(type, index) {
             if (type === true) {
                 const comleteTodo = this.needDoList.splice(index, 1)
                 this.completeList.push(...comleteTodo)
-                
+
             } else {
                 const noComleteTodo = this.completeList.splice(index, 1)
                 this.needDoList.push(...noComleteTodo)
